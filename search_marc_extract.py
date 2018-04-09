2d6e020da2d1f2717dabca517a93ab83ff73c619
Ori Hoch <ori@uumpa.com>
2018-04-09 18:31:01 +0300

    add marc extract processor to search_export pipeline - extracts item type fields

---
+++ search_marc_extract.py
@@ -0,0 +1,48 @@
+from datapackage_pipelines.wrapper import ingest, spew
+import logging, re, pymarc, json
+
+
+parameters, datapackage, resources, stats = ingest() + ({},)
+
+
+output_fields = parameters['fields']
+
+
+unique_records_resource_num = None
+
+
+for i, resource in enumerate(datapackage['resources']):
+    if resource['name'] == 'unique_records':
+        unique_records_resource_num = i
+        for output_field in output_fields:
+            datapackage['resources'][unique_records_resource_num]['schema']['fields'].append({'name': output_field['name'],
+                                                                                              'type': 'string'})
+
+
+def get_resource(resource):
+    for row in resource:
+        for output_field in output_fields:
+            if 'marc_tag' in output_field:
+                for record in pymarc.JSONReader(json.dumps([row['json']])):
+                    record_fields = record.get_fields(output_field['marc_tag'])
+                    if len(record_fields) > 0:
+                        record_field = record_fields[0]
+                        row[output_field['name']] = record_field.format_field()
+                        break
+                    else:
+                        row[output_field['name']] = ''
+            elif 'marc_leader_position' in output_field and 'marc_leader_map' in output_field:
+                key = row['json']['leader'][output_field['marc_leader_position']]
+                row[output_field['name']] = output_field['marc_leader_map'][key]
+        yield row
+
+
+def get_resources():
+    for i, resource in enumerate(resources):
+        if i == unique_records_resource_num:
+            yield get_resource(resource)
+        else:
+            yield resource
+
+
+spew(datapackage, get_resources(), stats)
